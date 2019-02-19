61ee2686cfcfb1b0377f5f3fe28cf2de209b029e
Faisal Manzer <faisal_manzer@yahoo.in>
2019-02-19 18:07:34 +0530

    Minor changes

---
+++ 02_control_flow/02_if_statement.py
@@ -0,0 +1,13 @@
+"""
+sec: 02
+topic: 02
+name: IF Statement
+author: Faisal Manzer
+"""
+
+"""If works with boolean."""
+if True:
+    print("Condition is always True")
+
+if False:
+    print("Will never run")
