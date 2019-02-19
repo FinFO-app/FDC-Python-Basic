44cd612a1ccb0b0bc6bfc9ca9ed217dcc9847d0a
Faisal Manzer <faisal_manzer@yahoo.in>
2019-02-20 01:12:09 +0530

    Minor changes

--- 02_control_flow/02_if_statement.py
+++ 02_control_flow/02_if_statement.py
@@ -11,3 +11,13 @@ if True:
 
 if False:
     print("Will never run")
+
+name = input("Enter you name: ")
+age = int(input("{} enter your age: ").format(name))
+
+if age >= 18:
+    print("You are old enough to vote")
+    print("We request you to cast your vote")
+else:
+    print("Sorry {}, you can't vote".format(name))
+    print("Wait for {} years to cast vote".format(18 - age))
