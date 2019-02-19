91d8b700cd1812bc55149c6f4d77354450997ee1
Faisal Manzer <faisal_manzer@yahoo.in>
2019-02-19 17:32:13 +0530

    Added Code Block

---
+++ 02_control_flow/01_code_block.py
@@ -0,0 +1,27 @@
+"""
+sec: 02
+topic: 01
+name: String Formatting
+author: Faisal Manzer
+"""
+
+"""Code Block"""
+
+# Code block in python is indented by whitespace
+# Generally we use TAB or 4 SPACES
+for i in range(1, 11):
+    print("{:2} squared = {:3}".format(i, i**2))
+
+# Multiple Blocks
+age = int(input("Enter Age: "))
+if age >= 18:
+    print("You can VOTE.")
+else:
+    print("You can't VOTE.")
+
+# Blocks in side Block
+for i in range(1, 11):
+    if i % 2:
+        print("{} is Even".format(i))
+    else:
+        print("{} is Odd".format(i))
